bfe85a605bc1a4113eb57fc5230d6f6f6d454b33
JoshEthan <joshuaeris@gmai.com>
2021-02-25 18:36:32 -0700

    Added basic stuff to agent file.

--- agent.py
+++ agent.py
@@ -1,1 +1,25 @@
-# Call play_step
+import torch
+import random
+import numpy as np
+from collections import deque
+from game import SnakeGame, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001 # Learning Rate
+
+class Agent:
+#   constructor
+#   get_state
+#   remember
+#   train_long_memory
+#   train_short_memory
+#   get_action
+
+# Training Function
+def train():
+    pass
+
+# ???
+if __name__ == '__main__':
+    train()
